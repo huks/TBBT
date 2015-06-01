52369ef9928806ec44f1e5fb95b526dd0bb3f3f3
Byung Joo Kim <huks7417@gmail.com>
2015-06-01 15:23:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var app = angular.module('tbbtApp', []);
+var realm = "Frostmourne";
+var guildName = "The Big Bad Theory";
+var apiKey = "998b45vfwkxvg6ftz5vurk8z789daq37";
 app.controller('tbbtCtrl', function($scope, $http) {
-	$http.get("https://us.api.battle.net/wow/guild/Frostmourne/The%20Big%20Bad%20Theory?fields=members&locale=en_US&apikey=998b45vfwkxvg6ftz5vurk8z789daq37")
+	$http.get("https://us.api.battle.net/wow/guild/"+realm+"/"+guildName+"?fields=members&locale=en_US&apikey="+apiKey)
 	.success(function (data) {
     var members = data.members;
     $scope.rowList = [];
-
-    for(i=0; i<members.length; i++){
-      $http.get("https://us.api.battle.net/wow/character/Frostmourne/"+members[i].character.name+"?fields=items&locale=en_US&apikey=998b45vfwkxvg6ftz5vurk8z789daq37")
+    
+    function getItemLvl(rowdata){
+      $http.get("https://us.api.battle.net/wow/character/"+realm+"/"+rowdata.character.name+"?fields=items&locale=en_US&apikey="+apiKey)
       .success(function (rowdata) {
-        if(rowdata.level == 100){
+        if(rowdata.level == 100 && rowdata.items.averageItemLevel >= 640){
           $scope.rowList.push(rowdata);
         }
       });
+    }
+
+    for(i=0; i<members.length; i++){
+      getItemLvl(members[i]);
     }
   });
 });
