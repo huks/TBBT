9377ee442d588e85a71a0cf9e7227bbbe8eec0b8
Byung Joo Kim <huks7417@gmail.com>
2015-06-03 14:30:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ var realm = "Frostmourne";
 var guildName = "The Big Bad Theory";
 var apiKey = "998b45vfwkxvg6ftz5vurk8z789daq37";
 
-// create the module
 var app = angular.module("tbbtApp", ["ngRoute"]);
 
-// configure routes
 app.config(function($routeProvider) {
   $routeProvider
-  .when("/ilvl", {
-    templateUrl: "ilvl.html",
-    controller: "ilvlCtrl"
-  })
+    .when("/ilvl", {
+      templateUrl: "ilvl.html",
+      controller: "ilvlCtrl"
+    })
     .when("/roster", {
       templateUrl: "roster.html",
       controller: "rosterCtrl"
@@ -19,66 +17,4 @@ app.config(function($routeProvider) {
     .otherwise({
       redirectTo: "/ilvl"
     });
-});
-
-// create the controller
-app.controller("ilvlCtrl", function($scope, $http) {
-  $http.get("https://us.api.battle.net/wow/guild/" + realm + "/" + guildName + "?fields=members&locale=en_US&apikey=" + apiKey)
-    .success(function(data) {
-      var members = data.members;
-      $scope.rowList = [];
-
-      function getItemLvl(rowdata) {
-        $http.get("https://us.api.battle.net/wow/character/" + realm + "/" + rowdata.character.name + "?fields=items&locale=en_US&apikey=" + apiKey)
-          .success(function(rowdata) {
-            if (rowdata.level == 100 && rowdata.items.averageItemLevel >= 640) {
-              $scope.rowList.push(rowdata);
-            }
-          });
-      }
-
-      for (i = 0; i < members.length; i++) {
-        getItemLvl(members[i]);
-      }
-    });
-
-  $scope.iLvlFilter = function(data) {
-    if (data < 655) {
-      return "warning"
-    } else {
-      return ""
-    }
-  }
-
-  $scope.getClass = function(data) {
-    if (data == 1) {
-      return "warrior"
-    } else if (data == 2) {
-      return "paladin"
-    } else if (data == 3) {
-      return "hunter"
-    } else if (data == 4) {
-      return "rogue"
-    } else if (data == 5) {
-      return "priest"
-    } else if (data == 6) {
-      return "deathknight"
-    } else if (data == 7) {
-      return "shaman"
-    } else if (data == 8) {
-      return "mage"
-    } else if (data == 9) {
-      return "warlock"
-    } else if (data == 10) {
-      return "monk"
-    } else if (data == 11) {
-      return "druid"
-    } else {
-      return ""
-    }
-  }
-});
-
-app.controller("rosterCtrl", function($scope) {
-  // TODO
 });
